90db9d90a663c2899541e8b0112a796e5105e272
kunterbunt <to-kunterbunt@gmx.net>
2015-06-13 21:00:37 +0200

    still initial

--- app/src/main/java/sebastians/challenge/MyChallengesFragment.java
+++ app/src/main/java/sebastians/challenge/MyChallengesFragment.java
@@ -2,6 +2,7 @@ package sebastians.challenge;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,7 @@ public class MyChallengesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.i("Test", "Test");
         return inflater.inflate(R.layout.fragment_my_challenges, container, false);
     }
 }
